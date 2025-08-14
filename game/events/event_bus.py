10ab82a1bb58ccf51600fe307b7cfcd5aec50acd
hungdi <hjh3757@naver.com>
2025-08-14 18:26:09 +0900

    dd

--- game/events/event_bus.py
+++ game/events/event_bus.py
@@ -5,8 +5,8 @@ from .events import Event, EventType
 class EventBus:
     def __init__(self):
         # 핸들러의 타입을 DefaultDict타입으로 정의한것, 비어있어도 defaultdict(list)로 처리.
-        # List[Callable[[Event], None]] : 값이 함수로 되어있다는 것을 정의한 것. 
-        # Callable[[Event], None] : 인자로 Event를 하나 받고, 반환값이 없는 함수. (generic의 상세구현을 의미함)
+        # List[Callable[[Event], None]] : 리스트의 요소가 값이 함수로 되어있다는 것을 정의한 것.
+        # Callable[[Event], None] : 인자로 Event를 하나 받고, 반환값이 없는 함수. ([,,]은 generic의 상세구현을 의미함)
         self._handlers: DefaultDict[EventType, List[Callable[[Event], None]]] = defaultdict(list)
 
     # 이벤트 핸들러 등록하기
