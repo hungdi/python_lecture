e617037711c2ac44acf44de8200d5070a9fc16c6
hungdi <hjh3757@naver.com>
2025-08-14 18:25:23 +0900

    add comment

--- game/events/event_bus.py
+++ game/events/event_bus.py
@@ -4,8 +4,14 @@ from .events import Event, EventType
 
 class EventBus:
     def __init__(self):
+        # 핸들러의 타입을 DefaultDict타입으로 정의한것, 비어있어도 defaultdict(list)로 처리.
+        # List[Callable[[Event], None]] : 값이 함수로 되어있다는 것을 정의한 것. 
+        # Callable[[Event], None] : 인자로 Event를 하나 받고, 반환값이 없는 함수. (generic의 상세구현을 의미함)
         self._handlers: DefaultDict[EventType, List[Callable[[Event], None]]] = defaultdict(list)
 
+    # 이벤트 핸들러 등록하기
+    # event_type에 해당하는 이벤트가 발생하면, 실행할 함수를 등록.
+    # 예: 고블린이 죽으면, on_enemy_died 함수를 호출해서 ScoreManager가 점수 +처리
     def subscribe(self, event_type: EventType, handler: Callable[[Event], None]) -> None:
         self._handlers[event_type].append(handler)
 
--- game/events/events.py
+++ game/events/events.py
@@ -4,7 +4,12 @@ from typing import Any
 
 class EventType(Enum):
     ENEMY_DIED = auto()
+    ENEMY_SPAWNED = auto()
 
+# frozen = True : 불변 객체로 만듬 (한번 만든 후 속성 변경 불가)
+# Any (타입제한없이 아무값이나 받을 수 있음), 기본값은 None
+# 사용의 예
+# event = Event(type=EventType.ENEMY_DIED, payload={"enemy_id": 42})
 @dataclass(frozen=True)
 class Event:
     type: EventType
